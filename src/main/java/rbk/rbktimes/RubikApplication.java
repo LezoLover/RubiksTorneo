733e7c615fa24ad1f93a13e3031f4cd65bbf62e1
Franco Garcia <72900907+LezoLover@users.noreply.github.com>
2023-02-26 20:10:58 -0600

    Create RubikApplication.java

---
+++ src/main/java/rbk/rbktimes/RubikApplication.java
@@ -0,0 +1,24 @@
+package rbk.rbktimes;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RubikApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RubikApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Rubik Torneo");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
